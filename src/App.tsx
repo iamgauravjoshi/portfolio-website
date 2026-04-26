80eb7fe26d20062f703ea0c789a39793f5663f47
Gaurav Joshi <gjoshi@okruti.com>
2026-04-26 11:05:45 +0530

    Created routes and fixed designing

--- src/App.tsx
+++ src/App.tsx
@@ -11,23 +11,23 @@ import Blogs from "./pages/Blogs";
 import ErrorPage from "./components/common/ErrorPage";
 
 function App() {
-   return (
-      <>
-         <Navbar />
-         <Routes>
-            <Route path='/' element={<Hero />} />
-            <Route path='/home' element={<Hero />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/projects' element={<Projects />} />
-            <Route path='/experience' element={<Experience />} />
-            <Route path='/blogs' element={<Blogs />} />
-            <Route path='/contact' element={<Contact />} />
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/home" element={<Hero />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/contact" element={<Contact />} />
 
-            {/* Redirect unknown routes */}
-            <Route path='*' element={<ErrorPage />} />
-         </Routes>
-      </>
-   );
+        {/* Redirect unknown routes */}
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
